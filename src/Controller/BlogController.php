f31e026f4360ce871f37d7c567cca646dd87a89b
Exal <depresles.nicolas@gmail.com>
2018-12-03 13:26:18 +0100

    created show.html.twig and links towards it

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -16,4 +16,12 @@ class BlogController extends AbstractController
             'controller_name' => 'BlogController',
         ]);
     }
+
+    /**
+     * @Route("/blog/12", name="blog_show")
+     */
+    public function trick_show()
+    {
+        return $this->render('blog/show.html.twig');
+    }
 }
