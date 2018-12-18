5fb20b3a233244309775a8c35fd5df749d73c2bf
Exal <depresles.nicolas@gmail.com>
2018-12-18 17:51:51 +0100

    Created a way to add a trick

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -3,8 +3,13 @@
 namespace App\Controller;
 
 use App\Repository\TrickRepository;
+use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use App\Entity\Trick;
 
 class BlogController extends AbstractController
@@ -19,6 +24,34 @@ class BlogController extends AbstractController
         return $this->render('blog/index.html.twig', [
             'controller_name' => 'BlogController',
             'tricks'  => $tricks
+        ]);
+    }
+
+    /**
+     * @Route("/blog/new", name="blog_create")
+     */
+    public function createTrick(Request $request, ObjectManager $manager)
+    {
+        $trick = new Trick();
+
+        $form = $this->createFormBuilder($trick)
+                     ->add('title')
+                     ->add('description')
+                     ->getForm();
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            $trick->setCreatedAt(new \DateTime());
+
+            $manager->persist($trick);
+            $manager->flush();
+
+            return $this->redirectToRoute('blog_show', ['id' => $trick->getId()]);
+        }
+
+        return $this->render('blog/create.html.twig', [
+            'formTrick' => $form->createView()
         ]);
     }
 
