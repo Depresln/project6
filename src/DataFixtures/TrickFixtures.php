0e2eb701fc7072861b4e0fed32a6e796cacca3d7
Exal <depresles.nicolas@gmail.com>
2019-01-08 17:08:48 +0100

    Added fixtures for tricks comments and categories using Faker

--- src/DataFixtures/TrickFixtures.php
+++ src/DataFixtures/TrickFixtures.php
@@ -5,18 +5,49 @@ namespace App\DataFixtures;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 use App\Entity\Trick;
+use App\Entity\Category;
+use App\Entity\Comment;
 
 class TrickFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        for($i = 1; $i <= 10; $i++){
+        $faker = \Faker\Factory::create('fr_FR');
+
+        // Create 3 categories
+        for($i = 1; $i <= 3; $i++){
+            $category = new Category();
+            $category->setTitle($faker->sentence());
+
+            $manager->persist($category);
+        }
+
+        // Create between 4 and 6 tricks with comments
+        for($j = 1; $j <= mt_rand(4, 6); $j++){
             $trick = new Trick();
-            $trick->setTitle("Titre du trick n°$i")
-                  ->setDescription("<p>Contenu du trick n°$i</p>")
-                  ->setCreatedAt(new \DateTime());
+
+            $content = '<p>' . join($faker->paragraphs(5), '</p><p>') . '</p>';
+
+            $trick->setTitle($faker->sentence())
+                  ->setDescription($content)
+                  ->setCreatedAt($faker->dateTimeBetween('-6 months'))
+                  ->setCategory($category);
 
             $manager->persist($trick);
+
+            for($k = 1; $k <= mt_rand(4,10); $k++){
+                $comment = new Comment();
+
+                $content = '<p>' . join($faker->paragraphs(2), '</p><p>') . '</p>';
+
+                $days = (new \DateTime())->diff($trick->getCreatedAt())->days;
+
+                $comment->setContent($content)
+                        ->setCreatedAt($faker->dateTimeBetween('-' . $days . ' days'))
+                        ->setTrick($trick);
+
+                $manager->persist($comment);
+            }
         }
 
         $manager->flush();
