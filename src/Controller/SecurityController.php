dcc8ac734e31ea1c689068431247352f237d0ceb
Exal <depresles.nicolas@gmail.com>
2019-01-22 15:11:32 +0100

    Started working on login

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -33,10 +33,20 @@ class SecurityController extends AbstractController
 
             $manager->persist($user);
             $manager->flush();
+
+            return $this->redirectToRoute('security_login');
         }
 
         return $this->render('security/registration.html.twig', [
             'form' => $form->createView()
         ]);
+    }
+
+    /**
+     * @Route("/login", name="security_login")
+     */
+    public function login()
+    {
+        return $this->render('security/login.html.twig');
     }
 }
