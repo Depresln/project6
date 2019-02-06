9630755c78af3cf05a582000291ba8a82acceb43
Exal <depresles.nicolas@gmail.com>
2019-02-06 14:44:21 +0100

    Fixed migration versions

--- src/Migrations/Version20190206131429.php
+++ src/Migrations/Version20190206131429.php
@@ -8,13 +8,16 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20190204152507 extends AbstractMigration
+final class Version20190206131429 extends AbstractMigration
 {
     public function up(Schema $schema) : void
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
+        $this->addSql('ALTER TABLE media ADD tricks_id INT DEFAULT NULL');
+        $this->addSql('ALTER TABLE media ADD CONSTRAINT FK_6A2CA10C3B153154 FOREIGN KEY (tricks_id) REFERENCES trick (id)');
+        $this->addSql('CREATE INDEX IDX_6A2CA10C3B153154 ON media (tricks_id)');
     }
 
     public function down(Schema $schema) : void
@@ -25,9 +28,7 @@ final class Version20190204152507 extends AbstractMigration
         $this->addSql('ALTER TABLE comment DROP FOREIGN KEY FK_9474526CA76ED395');
         $this->addSql('DROP INDEX IDX_9474526CA76ED395 ON comment');
         $this->addSql('ALTER TABLE media DROP FOREIGN KEY FK_6A2CA10C3B153154');
-        $this->addSql('ALTER TABLE media DROP FOREIGN KEY FK_6A2CA10CEA9FDD75');
         $this->addSql('DROP INDEX IDX_6A2CA10C3B153154 ON media');
-        $this->addSql('DROP INDEX IDX_6A2CA10CEA9FDD75 ON media');
-        $this->addSql('ALTER TABLE media DROP tricks_id, DROP media_id');
+        $this->addSql('ALTER TABLE media DROP tricks_id');
     }
 }
